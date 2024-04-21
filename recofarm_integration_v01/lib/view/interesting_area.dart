5a89b0cfcfca3b423ecf60611c0d20462d9a6f38
pulpilisory <pulpilisory@gmailcom>
2024-04-21 18:05:14 +0900

    pdg : 내 경작지 리스트 및 경작지로 이동하기 구현 시작

--- recofarm_integration_v01/lib/view/interesting_area.dart
+++ recofarm_integration_v01/lib/view/interesting_area.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -8,13 +9,13 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
   Author      : Forrest DongGeun Park. (PDG)
   Updates     : 
 	  2024.04.21 Sun by pdg
-		  - 
+		  - google map 추가 및 ios minimum version 14 로 변환 
+      - footter 디자인 + 내 관심 경작지로 이동하기 ( 우선 엘리베이트 버튼으로 구현 )
   Detail      : - 
 
 */
 class InterestingAreaPage extends StatefulWidget {
   const InterestingAreaPage({super.key});
-
 
   @override
   State<InterestingAreaPage> createState() => _InterestingAreaPageState();
@@ -23,17 +24,14 @@ class InterestingAreaPage extends StatefulWidget {
 class _InterestingAreaPageState extends State<InterestingAreaPage> {
   // properties
   late TextEditingController locationTfController;
-  late LatLng interestLoc; 
+  late LatLng interestLoc;
 
   @override
   void initState() {
     super.initState();
     locationTfController = TextEditingController();
-    // 초기 관심 지역 위도 경도 설정 
-    interestLoc = const LatLng(
-      37.5233273,
-      126.921252
-    );
+    // 초기 관심 지역 위도 경도 설정
+    interestLoc = const LatLng(37.5233273, 126.921252);
     print(interestLoc);
   }
 
@@ -44,10 +42,10 @@ class _InterestingAreaPageState extends State<InterestingAreaPage> {
         backgroundColor: Colors.amber[100],
         actions: [
           IconButton(
-            onPressed: (){
-              // 소재지 검색 함수 
-            }, 
-            icon: Icon(Icons.search_outlined))
+              onPressed: () {
+                // 소재지 검색 함수
+              },
+              icon: Icon(Icons.search_outlined))
         ],
         title: PreferredSize(
           preferredSize: Size.fromHeight(100),
@@ -64,12 +62,32 @@ class _InterestingAreaPageState extends State<InterestingAreaPage> {
           ),
         ),
       ),
-      body:  GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: interestLoc,
-          zoom: 16),
-        
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: GoogleMap(
+              initialCameraPosition:
+                  CameraPosition(target: interestLoc, zoom: 16),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("내 관심 경작지 리스트(combo box)"),
+                ElevatedButton(
+                  onPressed: (){
+                    // google map 이동 
+                  }, 
+                  child: const Text("경작지1"))
+                
+
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
