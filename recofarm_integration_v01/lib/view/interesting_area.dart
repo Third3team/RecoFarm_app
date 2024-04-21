a819d5145f5bc3a43af3b557e72b33ff572ea48c
pulpilisory <pulpilisory@gmailcom>
2024-04-21 18:32:05 +0900

    pdg : 위치권한 문의 기능 완료

--- recofarm_integration_v01/lib/view/interesting_area.dart
+++ recofarm_integration_v01/lib/view/interesting_area.dart
@@ -11,6 +11,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 	  2024.04.21 Sun by pdg
 		  - google map 추가 및 ios minimum version 14 로 변환 
       - footter 디자인 + 내 관심 경작지로 이동하기 ( 우선 엘리베이트 버튼으로 구현 )
+      - 내위치 권한 인증 및 내위치로 첫 지도 카메라 위치 이동 시키기 
+      - 위치 권한이 없을 경우 앱을 사용하지 못하는 쪽으로 설계를 하자. 
   Detail      : - 
 
 */
@@ -25,6 +27,7 @@ class _InterestingAreaPageState extends State<InterestingAreaPage> {
   // properties
   late TextEditingController locationTfController;
   late LatLng interestLoc;
+  late bool islocationEnable;
 
   @override
   void initState() {
@@ -32,7 +35,7 @@ class _InterestingAreaPageState extends State<InterestingAreaPage> {
     locationTfController = TextEditingController();
     // 초기 관심 지역 위도 경도 설정
     interestLoc = const LatLng(37.5233273, 126.921252);
-    print(interestLoc);
+    //print(interestLoc);
   }
 
   @override
@@ -62,31 +65,45 @@ class _InterestingAreaPageState extends State<InterestingAreaPage> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 2,
-            child: GoogleMap(
-              initialCameraPosition:
-                  CameraPosition(target: interestLoc, zoom: 16),
-            ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("내 관심 경작지 리스트(combo box)"),
-                ElevatedButton(
-                  onPressed: (){
-                    // google map 이동 
-                  }, 
-                  child: const Text("경작지1"))
-                
 
+      // check permission 함수는 future 함수이므로 future builder 를 사용하는 것이 좋다.
+      body: FutureBuilder<String>(
+        future: checkPermission(),
+        builder: (context, snapshot) {
+          // 권한 정보가 스냅샷에 없을때  혹은 커넥션을 기다리고 있을때
+          if (!snapshot.hasData &&
+              snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.data == "위치 권한이 허가 되었습니다.") {
+            return Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: GoogleMap(
+                    initialCameraPosition:
+                        CameraPosition(target: interestLoc, zoom: 16),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("내 관심 경작지 리스트(combo box)"),
+                      ElevatedButton(
+                          onPressed: () {
+                            // google map 이동
+                          },
+                          child: const Text("경작지1"))
+                    ],
+                  ),
+                ),
               ],
-            ),
-          ),
-        ],
+            );
+          }
+        },
       ),
     );
   }
@@ -94,5 +111,31 @@ class _InterestingAreaPageState extends State<InterestingAreaPage> {
   // Function
   location_find() async {
     final checkedPermission = await Geolocator.checkPermission();
+  }
+
+  // 내위치  파악 및 권한 설정
+  Future<String> checkPermission() async {
+    islocationEnable = await Geolocator.isLocationServiceEnabled();
+
+    if (!islocationEnable) {
+      return "위치 서비스를 활성화 해주세요.";
+    }
+    // 위치 권한 확인
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if (checkedPermission == LocationPermission.denied) {
+      // 권한 요청
+      checkedPermission = await Geolocator.requestPermission();
+      if (checkedPermission == LocationPermission.denied) {
+        return "위치 권한을 허가해주세요";
+      }
+    }
+    // 위치 권한 거절됨 ( 앱에서 재요청 불가 )
+    if (checkedPermission == LocationPermission.deniedForever) {
+      return "앱의 위치 권한을 설정에서 허가해주세요.";
+    }
+
+    // 위의 모든 조건이 통과되면 위치 권한 허가 가 완료 된것임.
+    return "위치 권한이 허가 되었습니다.";
   }
 } // END
